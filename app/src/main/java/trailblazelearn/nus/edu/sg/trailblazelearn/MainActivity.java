6c4ad805c9dc2e315b1903649bea423b9982e0b2
Mohammad Asif Waquar <asifwaquar@users.noreply.github.com>
2018-03-10 00:34:13 +0800

    version 1.2

--- app/src/main/java/trailblazelearn/nus/edu/sg/trailblazelearn/MainActivity.java
+++ app/src/main/java/trailblazelearn/nus/edu/sg/trailblazelearn/MainActivity.java
@@ -1,11 +1,14 @@
 package trailblazelearn.nus.edu.sg.trailblazelearn;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -14,34 +17,138 @@ import android.widget.Spinner;
 import android.widget.Toast;
 import android.content.Intent;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 public class MainActivity extends AppCompatActivity {
 
     private Spinner training_mod, spinner2;
-    private Button btnGoogleSubmit;
-    private Button btnFacebookSubmit;
+    SignInButton Button;
+    FirebaseAuth mAuth;
+
+
+    private final static int RC_SIGN_IN=2;
+     GoogleApiClient mGoogleApiClient;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LoginButton();
-    }
 
-    public void LoginButton() {
         training_mod = (Spinner) findViewById(R.id.trainingmode);
-        btnGoogleSubmit = (Button) findViewById(R.id.google_sign_in_button);
-        btnFacebookSubmit = (Button) findViewById(R.id.facebook_sign_in_button);
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
 
-        btnGoogleSubmit.setOnClickListener(new OnClickListener() {
+        signInButton.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, ModuleListActivity.class);
-                startActivity(i);
+               signIn();
 
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() !=null){
+                    Intent i = new Intent(MainActivity.this, ModuleListActivity.class);
+                    startActivity(i);
+                }else{
+
+                }
+
+            }
+        };
+
+
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,
+                        new GoogleApiClient.OnConnectionFailedListener() {
+                            @Override
+                            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // Configure Google Sign In
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            GoogleSignInResult result=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()) {
+                    GoogleSignInAccount account=result.getSignInAccount();
+                    firebaseAuthWithGoogle(account);
+            }else{
+                Toast.makeText(MainActivity.this, "Auth went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                           // updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Auth went wrong", Toast.LENGTH_SHORT).show();
+                          //  updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 }
--- app/src/main/java/trailblazelearn/nus/edu/sg/trailblazelearn/ModuleListActivity.java
+++ app/src/main/java/trailblazelearn/nus/edu/sg/trailblazelearn/ModuleListActivity.java
@@ -14,12 +14,16 @@ import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import trailblazelearn.nus.edu.sg.trailblazelearn.dummy.DummyContent;
 
@@ -44,11 +48,39 @@ public class ModuleListActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private List<DummyContent.DummyItem> cartList;
 
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module_list);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() ==null){
+                    Intent i = new Intent(ModuleListActivity.this, MainActivity.class);
+                    startActivity(i);
+                }else{
+
+                }
+
+            }
+        };
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -169,5 +201,58 @@ public class ModuleListActivity extends AppCompatActivity {
 
             }
         }
+
+
+
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        // Handle action bar actions click
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+               // try {
+               //     startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getPackageName())));
+               // } catch (android.content.ActivityNotFoundException anfe) {
+                 //   startActivity(new Intent(Intent.ACTION_VIEW,
+                 //           Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+              //  }
+                return true;
+            case R.id.action_logout:
+                logoutUser();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    protected void logoutUser() {
+        FirebaseAuth.getInstance().signOut();
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() ==null){
+                    Intent i = new Intent(ModuleListActivity.this, MainActivity.class);
+                    startActivity(i);
+                }else{
+
+                }
+
+            }
+        };
     }
 }
