10dc36021a198669ea993e2ac12b16f745bb87cd
Mohammad Asif Waquar <asifwaquar@users.noreply.github.com>
2018-03-25 05:43:52 +0800

    version 1.11

--- app/src/main/java/model/TrailStation.java
+++ app/src/main/java/model/TrailStation.java
@@ -5,42 +5,75 @@ package model;
  */
 
 public class TrailStation {
-
-    String trailstationid,trailid,glocation,name,instruction;
+    String trailstationid;
+    String trailid;
+    String glocation;
+    String stationname;
+    String instruction;
     int sequenceid;
-    public TrailStation() {
-    }
 
-    public TrailStation(String trailstationid, String trailid, String glocation, String name, String instruction, int sequenceid) {
+    public TrailStation(String trailstationid, String trailid, String glocation, String stationname, String instruction, int sequenceid) {
         this.trailstationid = trailstationid;
         this.trailid = trailid;
         this.glocation = glocation;
-        this.name = name;
+        this.stationname = stationname;
         this.instruction = instruction;
         this.sequenceid = sequenceid;
+    }
+
+
+    public TrailStation() {
     }
 
     public String getTrailstationid() {
         return trailstationid;
     }
 
+    public void setTrailstationid(String trailstationid) {
+        this.trailstationid = trailstationid;
+    }
+
     public String getTrailid() {
         return trailid;
+    }
+
+    public void setTrailid(String trailid) {
+        this.trailid = trailid;
     }
 
     public String getGlocation() {
         return glocation;
     }
 
-    public String getName() {
-        return name;
+    public void setGlocation(String glocation) {
+        this.glocation = glocation;
+    }
+
+    public String getStationname() {
+        return stationname;
+    }
+
+    public void setStationname(String stationname) {
+        this.stationname = stationname;
     }
 
     public String getInstruction() {
         return instruction;
     }
 
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
     public int getSequenceid() {
         return sequenceid;
     }
+
+    public void setSequenceid(int sequenceid) {
+        this.sequenceid = sequenceid;
+    }
+
+
+
+
 }
